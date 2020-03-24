0cb5c3f8c0213ae82cd207f523b8de9c6d87b9e4
Your Name <you@example.com>
2020-03-24 20:14:54 +0400

    Customization application/config/config.php, application/config/database.php

---
+++ 9_phpinfo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title>phpinfo().</title>
+</head>
+<SCRIPT language=JavaScript1.2 src="cpj/a_js.js" type="text/javascript"></SCRIPT>
+
+
+<body>
+
+<?php
+	phpinfo();
+?>
+
+</body>
+</html>
